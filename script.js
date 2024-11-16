53812eae31cb5198d30c41399a182aa46f46586b
Misamu12 <162633102+Misamu12@users.noreply.github.com>
2024-11-16 13:38:10 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let reseau = ["Orange","Airtel","Vodacom","Africel"]
+    let Giga = Math.floor(Math.random(
+        reseau.sort()
+        ))
+    
+        function add(Mega) {
+            let reseau = ["Orange","Airtel","Vodacom","Africel"]
+            Mega = Math.random(reseau)
+            return Mega
+        }
+
+        console.log(add())
